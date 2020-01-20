595593e6ce0ff8bc7962086c1111b0f338812679
likezhang <likezhang@tencent.com>
2020-01-21 01:49:45 +0800

    client code

--- client/client.py
+++ client/client.py
@@ -1,34 +1,81 @@
-from autoflow import AutoFlow
 from webdevice import WebDevice
+import requests
+import sys
 
 class Client:
-    def __init__(self):
-        self.flow_server = AutoFlow()
-        self.devices = []
+    def __init__(self, device):
+        self.device = device
+        self.flowname = None
+        self.session_id = None
 
-    def init_server(self, test_json_file):
-        self.flow_server.load_from_file(test_json_file)
+    def create_session(self, flowname):
+        self.flowname = flowname
+        resp = requests.post('http://127.0.0.1:8080/autoflow/create/{}'.format(flowname))
+        data = resp.json()
+        if data.get('Error', None) != 0:
+            printf("failed creating session")
+            return False
 
-    def add_device(self, device):
-        self.devices.append(device)
+        self.session_id = data['Result']
+        return True
+
 
     def execute(self):
+        action, params = self.query_next()
         while True:
-            step = self.query_server()
-            if step is None:
-                print("no step received. test is done")
+            if action == "":
+                self.stop()
                 break
-            self.perform_step(step)
+
+            result = self.perform_action(action, params)
+            if result is None:
+                action, params = self.send_error()
+            else:
+                action, params = self.query_next(result)
 
 
-    def query_server(self):
-        step = self.flow_server.get_next_step()
-        return step
+    def query_next(self, action_result = {}):
+        print(action_result)
+        url = 'http://127.0.0.1:8080/autoflow/next/{}'.format(self.session_id)
+        resp = requests.post(url,json = action_result)
+        data = resp.json()
+        action = data['Action']
+        params = data['Parameters']
+        return action, params
 
-    def perform_step(self, step):
-        pass
+    def perform_action(self, action, params):
+        try:
+            print(action)
+            print(params)
 
-client = Client()
-client.init_server("flow.json")
-client.add_device(WebDevice())
-client.execute()
+            method = getattr(self.device, action)
+            result = method(params)
+
+            return result
+        except Exception as err:
+            print(err)
+            return None
+
+    def send_error(self):
+        url = 'http://127.0.0.1:8080/autoflow/error/{}'.format(self.session_id)
+        data = requests.post(url).json()
+        action = data['Action']
+        params = data['Parameters']
+        return action, params
+
+
+    def stop(self):
+        print("stop")
+        url = 'http://127.0.0.1:8080/autoflow/stop/{}'.format(self.session_id)
+        requests.post(url)
+
+def main():
+    client = Client(WebDevice())
+    if not client.create_session(sys.argv[1]):
+        exit(1)
+
+    client.execute()
+
+
+if __name__ == "__main__":
+    main()
--- client/webdevice.py
+++ client/webdevice.py
@@ -1,22 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from basedevice import BaseDevice
 
-class WebDevice(BaseDevice):
+class WebDevice:
     def __init__(self):
-        self.device = None
+        self.driver = None
 
-    def create_device(device_name):
-        pass
+    def create_browser(self, params):
+        path = "./{}driver".format(params['value'])
+        self.driver = webdriver.Chrome(path)
+        return {}
 
-    def open_url(url):
-        pass
+    def open_url(self, params):
+        self.driver.get(params['value'])
+        return {}
 
-    def find_by_id(id):
-        pass
+    def find_by_id(self, params):
+        self.driver.find_element_by_id(params['id'])
+        return {}
 
-    def send_keys_to_id(id, value):
-        pass
+    def send_keys_to_id(self, params):
+        element = self.driver.find_element_by_id(params['id'])
+        element.send_keys(params['value'])
+        return {}
 
-    def click_on_id(id):
-        pass
+    def click_on_id(self, params):
+        element = self.driver.find_element_by_id(params['id'])
+        element.click()
+        return {}
+
+    def send_value_by_classname(self, params):
+        element = self.driver.find_element_by_class_name(params['classname'])
+        result = {}
+        result[params['resultKey']]=element.text
+        return result
+
